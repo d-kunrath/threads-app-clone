2f117712037ab31f3c634cdee3712a46f597bac3
Daniel Kunrath Albanez <dkunrath@gmail.com>
2023-09-27 21:12:49 -0300

    feat: onboarding + image file

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,42 @@
+import { Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
+
+import "../globals.css"
+import Topbar from '@/components/shared/Topbar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Bottombar from '@/components/shared/Bottombar';
+
+export const metadata: Metadata = {
+  title: 'Threads Clone',
+  description: 'A next.js 13 Meta Threads Application'
+}
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          <Topbar />
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">
+                {children}
+              </div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from '@clerk/themes';
 
 function Topbar() {
   return (
@@ -23,6 +24,7 @@ function Topbar() {
         </div>
         <OrganizationSwitcher 
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4"
             }
