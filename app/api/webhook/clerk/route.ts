a25d4b4043bb3a6ccaaac55f5590da6f290f8ed2
Daniel Kunrath Albanez <dkunrath@gmail.com>
2023-10-12 15:51:01 -0300

    shore: ignore ts errors on first build

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -46,7 +46,7 @@ export const POST: TPost = async (request: Request) => {
     "svix-signature": header.get("svix-signature"),
   };
 
-  // Activitate Webhook in the Clerk Dashboard.
+  // Activate Webhook in the Clerk Dashboard.
   // After adding the endpoint, you'll see the secret on the right side.
   const wh = new Webhook(process.env.NEXT_CLERK_WEBHOOK_SECRET || "");
 
